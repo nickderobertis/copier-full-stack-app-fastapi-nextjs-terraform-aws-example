da383016b0f6992bb972f73ef958fc794413e1cd
Nick DeRobertis <derobertis.nick@gmail.com>
2023-01-06 23:04:09 +0000

    Update flexlate templates
    copier-full-stack-app-fastapi-nextjs-terraform-aws: 01cce0be8979bcf157903b061dba77d1
    
    -------------------BEGIN FLEXLATE TRANSACTION-------------------
    {
      "type": "update",
      "target": null,
      "out_root": null,
      "data": [
        {
          "full_name": "copier-full-stack-app-fastapi-nextjs-terraform-aws-example",
          "short_description": "Example output for copier-full-stack-app-fastapi-nextjs-terraform-aws. See the template at https://github.com/nickderobertis/copier-full-stack-app-fastapi-nextjs-terraform-aws",
          "repo_name": "copier-full-stack-app-fastapi-nextjs-terraform-aws-example",
          "repo_username": "nickderobertis",
          "application_name": "App Example",
          "application_slug": "app-example",
          "application_abbreviation": "app",
          "package_name": "backend",
          "package_folder": "backend",
          "main_domain": "not-a-real-url.com",
          "sentry_organization_slug": "nick-derobertis",
          "main_branch_name": "main",
          "author": "Nick DeRobertis",
          "author_email": "derobertis.nick@gmail.com"
        }
      ],
      "id": "d89d453e-d6ab-44c0-b631-ade54f1b5eb2"
    }

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # copier-full-stack-app-fastapi-nextjs-terraform-aws-example
 
-Example output for copier-full-stack-app-fastapi-nextjs-terraform-aws
+Example output for copier-full-stack-app-fastapi-nextjs-terraform-aws. See the template at https://github.com/nickderobertis/copier-full-stack-app-fastapi-nextjs-terraform-aws
--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ REPO_MAINTAINERS = [
 ]
 
 # Short description of the package
-PACKAGE_SHORT_DESCRIPTION = "Example output for copier-full-stack-app-fastapi-nextjs-terraform-aws"
+PACKAGE_SHORT_DESCRIPTION = "Example output for copier-full-stack-app-fastapi-nextjs-terraform-aws. See the template at https://github.com/nickderobertis/copier-full-stack-app-fastapi-nextjs-terraform-aws"
 
 # Long description of the package for PyPI
 # Set to 'auto' to use README.md as the PyPI description
